bd43a43feee2ae2f204c795d004c857ec0cf61e7
Antoine <gourgue.antoine@esh64.fr>
2023-09-17 03:30:11 +0200

    feat: create pokemon card and pokemon grid

---
+++ components/pokemon-grid.tsx
@@ -0,0 +1,42 @@
+"use client"
+import {useState} from "react";
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import {PokemonCard} from "@/components/pokemon-card";
+
+export function PokemonGrid() {
+    const [searchText, setSearchText] = useState((""));
+
+    return (
+        <>
+            <div>
+                <h3 className="text-2xl py-6 text-center">Search For Your Pokemon !</h3>
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="pokemonName">Pokemon Name</Label>
+                    <Input
+                        type="text"
+                        value={searchText}
+                        autoComplete="off"
+                        id="pokemonName"
+                        placeholder="Charizard, Pikachu, etc."
+                        onChange={(e) => setSearchText(e.target.value)}
+                    />
+
+                </div>
+                <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon Collection</h3>
+            </div>
+
+            <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
+
+                <PokemonCard name="Pikachu"
+                />
+                <PokemonCard name="Pikachu"
+                />
+                <PokemonCard name="Pikachu"
+                />
+
+
+            </div>
+        </>
+    )
+}
