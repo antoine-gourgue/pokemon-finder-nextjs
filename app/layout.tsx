37db9ed75004f43e83cee80fceb3e638dbade65c
Antoine <gourgue.antoine@esh64.fr>
2023-09-17 07:01:07 +0200

    feat: get pokemon image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import {ThemeProvider} from "@/components/theme-provider";
+import Link from "next/link";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -22,7 +23,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark">
             <main className="flex min-h-screen flex-col items-center p-24">
                 <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
-                    <h2 className="text-2xl text-bold">PokemonFinder</h2>
+                    <Link href="/"><h2 className="text-2xl text-bold">PokemonFinder</h2></Link>
                 </div>
 
                 {children}
